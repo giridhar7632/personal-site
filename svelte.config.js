e3a533d62f6bf834763377eb9224cf8fa8f1d6f5
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-03-02 09:13:08 +0000

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({})
 	},
 	preprocess: vitePreprocess()
 }
