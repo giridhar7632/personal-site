5e2722abff940ac8ecc3e4a98fcb9cbf0af49c4f
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-03-02 13:20:27 +0530

    add 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores'
+	import Button from '../components/common/Button.svelte'
+</script>
+
+<div class="flex my-auto w-full flex-col items-center justify-center">
+	<h1 class="text-8xl font-black">{$page.status}</h1>
+	<h3 class="my-4 text-xl">{$page.error.message}</h3>
+	<a href={'/'}>
+		<Button>Go to Home</Button>
+	</a>
+</div>
